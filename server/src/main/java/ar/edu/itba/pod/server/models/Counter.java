b76a160ba5da605725994f1f39b11c4c697bef87
Axel Preiti Tasat <axel.preitit@gmail.com>
2024-04-13 19:54:47 -0300

    implement counter model
    part of the code is commented as it needs other classes to implementate their methods

--- server/src/main/java/ar/edu/itba/pod/server/models/Counter.java
+++ server/src/main/java/ar/edu/itba/pod/server/models/Counter.java
@@ -1,4 +1,25 @@
 package ar.edu.itba.pod.server.models;
 
-public class Counter {
+import lombok.Getter;
+
+@Getter
+public class Counter implements Comparable<Counter> {
+
+    private final int id;
+    // private final CheckInHistory checkInHistory;
+
+    public Counter(int id) {
+        this.id = id;
+        // this.checkInHistory = checkInHistory;
+    }
+
+    @Override
+    public int compareTo(Counter o) {
+        return Integer.compare(this.id, o.id);
+    }
+
+    public void checkIn(Passenger passenger, Sector sector) {
+        // passenger.setCounter(this);
+        // checkInHistory.add(passenger, sector, this);
+    }
 }
