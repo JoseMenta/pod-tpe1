f07150b2894875f202909b97a73d4752f951e278
Axel Preiti Tasat <axel.preitit@gmail.com>
2024-04-13 21:01:50 -0300

    add log4j to counter

--- server/src/main/java/ar/edu/itba/pod/server/models/Counter.java
+++ server/src/main/java/ar/edu/itba/pod/server/models/Counter.java
@@ -1,10 +1,14 @@
 package ar.edu.itba.pod.server.models;
 
 import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Getter
 public class Counter implements Comparable<Counter> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Counter.class);
+
+    @Getter
     private final int id;
     // private final CheckInHistory checkInHistory;
 
@@ -13,13 +17,24 @@ public class Counter implements Comparable<Counter> {
         // this.checkInHistory = checkInHistory;
     }
 
+//    public CheckInHistory getCheckInHistory() {
+//        return checkInHistory.copy();
+//    }
+
     @Override
     public int compareTo(Counter o) {
         return Integer.compare(this.id, o.id);
     }
 
+    /**
+     * Check in a passenger in this counter
+     *
+     * @param passenger the passenger to check in
+     * @param sector the sector where the passenger is checking in
+     */
     public void checkIn(Passenger passenger, Sector sector) {
-        // passenger.setCounter(this);
+        // passenger.checkIn(this);
         // checkInHistory.add(passenger, sector, this);
+        // LOGGER.info("Passenger {} checked in at counter {} in sector {}", passenger.getBooking(), id, sector.getName());
     }
 }
