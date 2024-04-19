94b3c0683025cd6c8bf05736333f666d8cd6201b
JoseMenta <jrmenta2@gmail.com>
2024-04-19 20:56:44 -0300

    add implementation for CheckinsAction

--- client/src/main/java/ar/edu/itba/pod/client/queryClient/QueryActions.java
+++ client/src/main/java/ar/edu/itba/pod/client/queryClient/QueryActions.java
@@ -1,14 +1,12 @@
 package ar.edu.itba.pod.client.queryClient;
 
 import ar.edu.itba.pod.client.Action;
-import ar.edu.itba.pod.client.adminClient.AddSectorAction;
-import ar.edu.itba.pod.client.adminClient.AdminActions;
 
 import java.util.List;
 
 public enum QueryActions {
     COUNTERS("counters", new CountersAction(List.of(CountersAction.OUTPATH, CountersAction.SECTOR))),
-    CHECKINS("checkins", new CheckinsAction(List.of(CheckinsAction.OUTPATH, CheckinsAction.SECTOR, CheckinsAction.AIRLINE)));
+    CHECKINS("checkins", new CheckinsAction(List.of(CheckinsAction.OUT_PATH, CheckinsAction.SECTOR, CheckinsAction.AIRLINE)));
 
     private final String actionName;
     private final Action action;
