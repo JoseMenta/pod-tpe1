365c7c619b5f9c9b92e4b5d8de13f87820bc19a4
Gaston Francois <gfrancois@itba.edu.ar>
2024-04-19 19:17:48 -0300

    initial countersAction

--- client/src/main/java/ar/edu/itba/pod/client/queryClient/CountersAction.java
+++ client/src/main/java/ar/edu/itba/pod/client/queryClient/CountersAction.java
@@ -1,10 +1,16 @@
 package ar.edu.itba.pod.client.queryClient;
 
 import ar.edu.itba.pod.client.Action;
+import ar.edu.itba.pod.grpc.counter.CounterServiceGrpc;
+import ar.edu.itba.pod.grpc.query.CheckInStatusRequest;
+import ar.edu.itba.pod.grpc.query.CheckInStatusResponse;
+import ar.edu.itba.pod.grpc.query.QueryServiceGrpc;
 import io.grpc.ManagedChannel;
+import io.grpc.stub.StreamObserver;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 public class CountersAction extends Action {
     public static final String OUTPATH = "outPath";
@@ -15,7 +21,31 @@ public class CountersAction extends Action {
     }
 
     @Override
-    public void run(ManagedChannel channel) {
+    public void run(ManagedChannel channel) throws InterruptedException {
         Map<String, String> arguments = parseArguments();
+
+        final CountDownLatch finishLatch = new CountDownLatch(1);
+        QueryServiceGrpc.QueryServiceStub stub = QueryServiceGrpc.newStub(channel);
+
+        final StreamObserver<CheckInStatusResponse> observer = new StreamObserver<CheckInStatusResponse>() {
+            @Override
+            public void onNext(final CheckInStatusResponse checkIn) {
+                //TODO
+            }
+
+            @Override
+            public void onError(final Throwable throwable) {
+                finishLatch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                finishLatch.countDown();
+            }
+        };
+
+
+        stub.checkInStatus(CheckInStatusRequest.newBuilder().setSector(arguments.get(SECTOR)).build(), observer);
+        finishLatch.await();
     }
 }
